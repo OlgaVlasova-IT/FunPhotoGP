812f31d6a67b2ceba6e190ef8e021377bd032bf4
Leonid Zakharov <LZakharov2@slb.com>
2023-11-14 19:55:20 -0700

    2023-11-13 7:55pm

--- js/portfolio.js
+++ js/portfolio.js
@@ -15,15 +15,31 @@
 //   });
 // }
 
-let items = document.querySelectorAll('.carousel .carousel-item')
-
-items.forEach((el) => {
+let itemsA = document.querySelectorAll('.angels')
+console.log(itemsA)
+itemsA.forEach((el) => {
     const minPerSlide = 4
     let next = el.nextElementSibling
     for (var i=1; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
-        	next = items[0]
+        	next = itemsA[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+
+let itemsW = document.querySelectorAll('.witch')
+
+itemsW.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = itemsW[0]
       	}
         let cloneChild = next.cloneNode(true)
         el.appendChild(cloneChild.children[0])
@@ -32,6 +48,21 @@ items.forEach((el) => {
 })
 
 
+let itemsH = document.querySelectorAll('.halloween')
+
+itemsH.forEach((el) => {
+    const minPerSlide = 4
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = itemsH[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
 // let itemsW = document.querySelectorAll('.witch')
 
 // itemsW.forEach((el) => {
